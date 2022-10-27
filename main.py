c28e6d5533f40199fd4af7afda6bc51e5d4ffc07
Achraf-Youssef <100535820+Achraf-Youssef@users.noreply.github.com>
2022-10-27 11:10:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ DEFAULT_HEIGHT = 1080
 WIDTH = 1024
 HEIGHT = 720
 SCALE = (DEFAULT_WIDTH/WIDTH) / (DEFAULT_HEIGHT/HEIGHT)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 # screen + caption
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
 pygame.display.set_caption("Pong!")
 
 # variables
@@ -20,9 +22,13 @@ pygame.display.set_caption("Pong!")
 # main loop
 run = True
 while run:
+    screen.fill(BLACK)
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+            
+    pygame.display.update()
 
 # quit the game
 pygame.quit()
